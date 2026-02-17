4ce9b572d02e5f28f5a739057d397c3e857a623a
Jacob-Jan <mosselman.jacobjan@gmail.com>
2026-02-17 20:51:58 +0100

    Wire legacy static pages into routes and navigation

--- proof-of-heart-nostr/src/app/app.routes.ts
+++ proof-of-heart-nostr/src/app/app.routes.ts
@@ -2,23 +2,23 @@ import { Routes } from '@angular/router';
 import { CharitiesComponent } from './charities/charities';
 import { CharityDetailComponent } from './charity-detail/charity-detail';
 import { ProfileEditorComponent } from './profile-editor/profile-editor';
-import { AboutusComponent } from './static/aboutus';
-import { PaperComponent } from './static/paper';
-import { BitcoinDonationsComponent } from './static/bitcoin-donations';
-import { BitcoinCharitiesComponent } from './static/bitcoin-charities';
-import { ProofOfHeartStaticComponent } from './static/proof-of-heart';
-import { PartnerComponent } from './static/partner';
+import { PaperPageComponent } from './static/paper-page';
+import { AboutPageComponent } from './static/about-page';
+import { PartnerPageComponent } from './static/partner-page';
+import { SeoCharitiesPageComponent, SeoDonationsPageComponent, SeoProofPageComponent } from './static/seo-pages';
 
 export const routes: Routes = [
   { path: '', component: CharitiesComponent },
   { path: 'charity/:pubkey', component: CharityDetailComponent },
   { path: 'profile', component: ProfileEditorComponent },
   { path: 'signup', component: ProfileEditorComponent },
-  { path: 'aboutus', component: AboutusComponent },
-  { path: 'paper', component: PaperComponent },
-  { path: 'bitcoin-donations', component: BitcoinDonationsComponent },
-  { path: 'bitcoin-charities', component: BitcoinCharitiesComponent },
-  { path: 'proof-of-heart', component: ProofOfHeartStaticComponent },
-  { path: 'partner', component: PartnerComponent },
+
+  { path: 'paper', component: PaperPageComponent },
+  { path: 'aboutus', component: AboutPageComponent },
+  { path: 'bitcoin-charities', component: SeoCharitiesPageComponent },
+  { path: 'bitcoin-donations', component: SeoDonationsPageComponent },
+  { path: 'proof-of-heart', component: SeoProofPageComponent },
+  { path: 'partner', component: PartnerPageComponent },
+
   { path: '**', redirectTo: '' }
 ];
